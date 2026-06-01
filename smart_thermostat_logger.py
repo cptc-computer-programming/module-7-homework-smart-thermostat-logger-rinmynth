a6cf6ece9389747b6db14f15f8997f9c6d20100e
Ray Stallard <rinmynth@gmail.com>
2026-05-31 18:02:58 -0700

    have added user input and added an if statement to validate the input

--- smart_thermostat_logger.py
+++ smart_thermostat_logger.py
@@ -3,3 +3,16 @@ MAX_VALID_TEMP = 100
 
 COLD_LIMIT = 68
 WARM_LIMIT = 76
+
+# variable for invalid value
+invalid_value = 0
+
+# ask user for number of temperature readings
+user_temperature_readings = int(input("Enter the number of temperature readings: "))
+
+# checks if user input is valid
+if user_temperature_readings <= invalid_value:
+    print("Error: The number of readings must be greater than 0")
+    user_temperature_readings
+else:
+    print("good")
